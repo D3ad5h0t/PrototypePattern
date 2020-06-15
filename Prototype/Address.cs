eb975e1c45acf727000b4c47e7845ff34c2754f7
D3ad5h0t <alexgl007@mail.ru>
2020-06-15 10:06:57 +1000

    prototype pattern

---
+++ Prototype/Address.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Prototype
+{
+    public class Address : ICloneable
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
+
+        public object Clone()
+        {
+            return new Address(StreetName, HouseNumber);
+        }
+    }
+}
